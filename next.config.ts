1251eefd4a3947f91bc1cc8d0852b57118f519a7
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2024-12-29 00:27:06 +0530

    🖥️ wip: adding images to hero-slider

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,11 @@ const nextConfig: NextConfig = {
         hostname: "drive.google.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
     ],
   },
 };
