3d90cad65114b1bc056dfa3d5dfd6ef24517f2ad
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2025-01-27 22:54:56 +0530

    ✨feature: tree-info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "./components/hero-section";
 import NewsSection from "./components/news";
+import IntroContent from "./components/tree-info";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <HeroSection />
+      <IntroContent />
       <NewsSection />
     </div>
   );
