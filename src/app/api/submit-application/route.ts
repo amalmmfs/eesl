0453479f4077a678e09033055148d9b6799d0662
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2025-02-21 02:07:11 +0530

    🐞 fix: edge runtime cleanup

--- src/app/api/submit-application/route.ts
+++ src/app/api/submit-application/route.ts
@@ -97,4 +97,4 @@ export async function POST(req: Request) {
   }
 }
 
-export const runtime = "edge"; // Better performance with Edge runtime
+// export const runtime = "edge"; // Better performance with Edge runtime
